e2a01816f0f80f16eece32bca9f1dde52f8a7d44
bronzelegs <tdavis0525@gmail.com>
2018-01-14 11:52:39 -0800

    sufficient testing, POST, PUT, DELETE, GET

---
+++ spec/test1Spec.js
@@ -0,0 +1,17 @@
+let request = require("request");
+let server = require("../server.js")
+let base_url = "http://localhost:3000/"
+
+describe("test welcome message", function() {
+  describe("GET /", function() {
+    it("returns status code 200", function(done) {
+      request.get(base_url, function(error, response, body) {
+        expect(response.statusCode).toBe(200);
+        let json = JSON.parse(body);
+        expect(json.message).toBe("Welcome to triple stores on mongo.");
+        console.log(json);
+        done();
+      });
+    });
+  });
+});
---
+++ spec/test2Spec.js
@@ -0,0 +1,58 @@
+let request = require("request");
+let server = require("../server.js")
+let base_url = "http://localhost:3000/triples"
+let createdId = "";
+
+describe("create", function() {
+  describe("POST /triples", function() {
+
+    it("returns status code 200", function(done) {
+      request({
+        url: base_url,
+        method: 'POST',
+        json: {
+          subj: 'posttest',
+          pred: 'test',
+          obj: 'testing'
+        }
+      }, function(error, response, body) {
+
+        //let json= JSON.parse(body[0]);
+        createdId = body._id;
+        console.log('POST', body);
+        //request.get(base_url, function(error, response, body) {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+    });
+  });
+  describe("PUT /triples id", function() {
+    it("returns status code 200", function(done) {
+      request({
+        url: base_url + '/' + createdId,
+        method: 'PUT',
+        json: {
+          subj: 'puttest',
+          pred: 'test',
+          obj: 'testing'
+        }
+      }, function(error, response, body) {
+        console.log("PUT", body);
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+    });
+  });
+  describe("DELETE /triples id", function() {
+    it("returns status code 200", function(done) {
+      request({
+        url: base_url + '/' + createdId,
+        method: 'DELETE'
+      }, function(error, response, body) {
+        console.log(error, body);
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+    });
+  });
+});

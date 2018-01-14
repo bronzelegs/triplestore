cd451037aaa7328dfacb4dc604b73938485079cb
bronzelegs <tdavis0525@gmail.com>
2018-01-14 11:53:05 -0800

    sufficient testing, POST, PUT, DELETE, GETALL

---
+++ spec/test3Spec.js
@@ -0,0 +1,17 @@
+let request = require("request");
+let server = require("../server.js")
+let base_url = "http://localhost:3000/triples"
+
+describe("get a bunch", function() {
+  describe("GET /triples", function() {
+    it("returns status code 200", function(done) {
+      request.get(base_url, function(error, response, body) {
+        expect(response.statusCode).toBe(200);
+        let json = JSON.parse(body);
+        expect(json);
+        console.log(json);
+        done();
+      });
+    });
+  });
+});
